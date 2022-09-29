9545f165aebf11e80a851a46df7989e59e707b6b
conchonha <thaibasang8@gmail.com>
2022-09-29 07:25:45 +0700

    update Base navigator_services.dart

--- flutter_library/lib/utils/navigator_services.dart
+++ flutter_library/lib/utils/navigator_services.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class NavigatorServices {
   static final _instance = NavigatorServices._internal();
-  GlobalKey<NavigatorState>? _globalKey;
+  GlobalKey<NavigatorState>? globalKey;
 
   NavigatorServices._internal();
 
@@ -12,24 +12,24 @@ class NavigatorServices {
   }
 
    void initNavigator(GlobalKey<NavigatorState> navigator) {
-    _globalKey = navigator;
+    globalKey = navigator;
   }
 
   push(Widget page) {
     _checkGlobalState();
     Navigator.push(
-        _globalKey!.currentContext!, MaterialPageRoute(builder: (ct) => page));
+        globalKey!.currentContext!, MaterialPageRoute(builder: (ct) => page));
   }
 
   pushName(String name,{Object? argument}) {
     _checkGlobalState();
-    Navigator.pushNamed(_globalKey!.currentContext!, name,arguments: argument);
+    Navigator.pushNamed(globalKey!.currentContext!, name,arguments: argument);
   }
 
   pushAndRemoveUtil(Widget page,String removeToName) {
     _checkGlobalState();
     Navigator.pushAndRemoveUntil(
-        _globalKey!.currentContext!,
+        globalKey!.currentContext!,
         MaterialPageRoute(builder: (BuildContext context) => page),
         ModalRoute.withName(removeToName));
   }
@@ -37,41 +37,41 @@ class NavigatorServices {
   pushNameAndRemoveUtil(String name,removeToName,{Object? argument}) {
     _checkGlobalState();
     Navigator.pushNamedAndRemoveUntil(
-        _globalKey!.currentContext!, name, ModalRoute.withName(removeToName),arguments: argument);
+        globalKey!.currentContext!, name, ModalRoute.withName(removeToName),arguments: argument);
   }
 
   pushReplacement(Widget page) {
     _checkGlobalState();
     Navigator.pushReplacement(
-        _globalKey!.currentContext!, MaterialPageRoute(builder: (BuildContext context) => page));
+        globalKey!.currentContext!, MaterialPageRoute(builder: (BuildContext context) => page));
   }
 
   pushReplacementNamed(String name,{Object? argument}) {
     _checkGlobalState();
     Navigator.pushReplacementNamed(
-        _globalKey!.currentContext!, name,arguments: argument);
+        globalKey!.currentContext!, name,arguments: argument);
   }
 
   pop() {
     _checkGlobalState();
     Navigator.pop(
-        _globalKey!.currentContext!);
+        globalKey!.currentContext!);
   }
 
   popUntil(String name) {
     _checkGlobalState();
     Navigator.popUntil(
-        _globalKey!.currentContext!,ModalRoute.withName(name));
+        globalKey!.currentContext!,ModalRoute.withName(name));
   }
 
   popAndPushNamed(String name,{Object? argument}) {
     _checkGlobalState();
     Navigator.popAndPushNamed(
-        _globalKey!.currentContext!,name,arguments: argument);
+        globalKey!.currentContext!,name,arguments: argument);
   }
 
   _checkGlobalState() {
-    if (_globalKey == null) {
+    if (globalKey == null) {
       throw ("please add NavigatorServices.initNavigator on main.dart ");
     }
   }
